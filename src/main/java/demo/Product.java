e97d337f496ea485883c8ca9ee3d41ca54b7d841
siva lokesh chilukuri <chilukurisivalokesh@gmail.com>
2018-12-12 21:10:09 +0530

    Version 1.0

---
+++ src/main/java/demo/Product.java
@@ -0,0 +1,15 @@
+package demo;
+
+/**
+ * Created by marwane on 16/02/17.
+ */
+public class Product {
+    public String name;
+
+    public Product(String name){
+        this.name = name;
+    }
+
+    public Product() {
+    }
+}
